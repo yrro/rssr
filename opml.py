ee07e8cedddc67b50374193532908f74521289d2
Sam Morris <sam@robots.org.uk>
2007-12-05 19:15:51 +0000

    Add OPML importer.

---
+++ opml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import xml.dom.minidom
+
+import db
+
+d = xml.dom.minidom.parse (sys.argv[1])
+
+opml = d.firstChild
+if opml.tagName != 'opml':
+	raise Exception ('not an OPML document')
+
+body = None
+for n in opml.childNodes:
+	if n.tagName == 'body':
+		body = n
+		break
+if body == None:
+	raise Exception ('no body element')
+
+outlines = [n for n in body.childNodes if n.nodeType == xml.dom.minidom.Node.ELEMENT_NODE and n.tagName == 'outline']
+
+s = db.Session ()
+for o in outlines:
+	f = db.Feed (o.getAttribute ('xmlUrl'))
+	s.save (f)
+s.commit ()
